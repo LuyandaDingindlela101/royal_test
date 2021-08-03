8081e452c91874d3110311a99cbed1f05c379ee4
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2021-08-03 22:34:31 +0200

    added form switch functionality

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,5 +1,21 @@
-fetch("http://127.0.0.1:5000/show-products/")
-.then(responce => responce.json())
-.then(data => {
-    console.log(data);
-})
+// fetch("http://127.0.0.1:5000/show-products/")
+// .then(responce => responce.json())
+// .then(data => {
+//     console.log(data);
+// })
+
+let login_btn = document.querySelector('.login-btn');
+let signup_btn = document.querySelector('.register-btn');
+
+let login_form = document.querySelector(".login");
+let register_form = document.querySelector(".register");
+
+login_btn.addEventListener('click', () => {
+    login_form.classList.toggle("inactive");
+    register_form.classList.toggle("inactive");
+});
+
+signup_btn.addEventListener('click', () => {
+    login_form.classList.toggle("inactive");
+    register_form.classList.toggle("inactive");
+});
